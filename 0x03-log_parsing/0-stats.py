35b0b8cc12c86f7de2d99bbbbe7020215690a1d9
tAf <muzirat0@gmail.com>
2024-07-18 13:38:01 +0000

    parse logs

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+log parser module
+"""
+import sys
+
+
+def print_stats(codes, tot_size):
+    print("File size: {}".format(tot_size))
+    for k, v in codes.items():
+        print("{}: {}".format(k, v))
+
+
+if __name__ == "__main__":
+    counter = 0
+    tot_size = 0
+    codes = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0
+    }
+
+    try:
+        for line in sys.stdin:
+            data = line.rstrip().split(' ')[-2:]
+            s_code = data[0]
+            f_size = int(data[1])
+
+            codes[s_code] += 1
+            tot_size += f_size
+
+            counter += 1
+            if counter == 10:
+                print_stats(codes, tot_size)
+                counter = 0
+    except KeyboardInterrupt:
+        print_stats(codes, tot_size)
