bbb0064cb95eccf390751dd746d44d1910cb4be9
tAf <muzirat0@gmail.com>
2024-08-22 22:22:58 +0000

    readme

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Make Change module
+"""
+
+
+def makeChange(coins, total):
+    """
+    TODO: Make Change algoritm
+    """
+    if total <= 0:
+        return 0
+    return -1
